9c33e3949467d40a6dec35667a32e13d8f66e8c7
Nick Herrick <nth.herrick@gmail.com>
2019-12-08 19:57:38 -0500

    finished remaining installs, added working code for axios get request to omdbapi, added a working user name and password input validation with inquirer

--- liri.js
+++ liri.js
@@ -1,14 +1,63 @@
 require("dotenv").config();
 const axios = require('axios');
-var inquirer = require('inquirer');
-var fs = require("fs");
-var moment = require('moment');
+const inquirer = require('inquirer');
+const fs = require("fs");
+const moment = require('moment');
 moment().format();
-var spotify = require('node-spotify-api');
-var keys = require("./keys.js");
-var spotifyKeys = new Spotify(keys.spotify);
-
-// var omdbURL = "http://www.omdbapi.com/?apikey[d81871c0]&"
+const spotifyAPI = require('node-spotify-api');
+const keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 
 // var inputCommand = process.argv[2];
 // var inputParameter = process.argv[3];
+
+const login = inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "Please Enter Username",
+        validate: function(input){
+            if(input === "username"){
+                return true;
+            }else if (input === ""){
+                return "Please Enter Username"
+            }else{
+                return "Incorrect Username"
+            }
+        }
+    },
+    {
+        type: "password",
+        name: "password",
+        message: "Please Enter Password",
+        validate: function(input){
+            if(input === "password"){
+                return true;
+            }else if (input === ""){
+                return "Please Enter Password"
+            }else{
+                return "Incorrect Password"
+            }
+        }
+    }
+]);
+login
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+    function(response){
+        console.log("The Movie's rating is: " + response.data.imdbRating);
+}).catch(function(error){
+    if(error.response){
+        console.log("---------------Data---------------");
+    console.log(error.response.data);
+    console.log("---------------Status---------------");
+    console.log(error.response.status);
+    console.log("---------------Status---------------");
+    console.log(error.response.headers);
+    }else if(error.request){
+        console.log(error.request);
+    }else{
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+});
