d9b39e81134016a80da4d774b0c3d2134412e1ef
Nick Herrick <nth.herrick@gmail.com>
2019-12-08 22:52:37 -0500

    added info to omdb function, added functionality that waits for more user commands after a command is entered rather than exiting and does so without asking for password and name again

--- liri.js
+++ liri.js
@@ -7,25 +7,8 @@ moment().format();
 const spotifyAPI = require('node-spotify-api');
 const keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
-
-// var inputCommand = process.argv[2];
-// var inputParameter = process.argv[3];
-
-const login = inquirer.prompt([
-    {
-        type: "input",
-        name: "username",
-        message: "Please Enter Username",
-        validate: function(input){
-            if(input === "username"){
-                return true;
-            }else if (input === ""){
-                return "Please Enter Username"
-            }else{
-                return "Incorrect Username"
-            }
-        }
-    },
+function begin(){
+    inquirer.prompt([
     {
         type: "password",
         name: "password",
@@ -40,24 +23,67 @@ const login = inquirer.prompt([
             }
         }
     }
-]);
-login
-
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+]).then(function getName(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Welcome! What's Your Name?",
+        }
+    ]).then(function welcome(input){
+        inquirer.prompt([
+            {
+                type: "confirm",
+                name: "continue",
+                message: "Welcome " + input.name + "! I'm Liri Bot. You can ask me to search for data about concerts, songs and movies. Would You Like to Continue?"
+            }
+    ]).then(function getCommand(){
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "command",
+        message: "Please Enter a Valid Input Command, or type 'help' for a list of commands"
+    }
+]).then(function(input){
+    var userInput = input.command;
+    var inputArray = userInput.split(" ");
+    var command = inputArray[0]
+    var searchTerm = inputArray.slice(1).join("+");
+    if(command === "movie-this"){
+        axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy").then(
     function(response){
-        console.log("The Movie's rating is: " + response.data.imdbRating);
+        console.log("Title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("imdb Rating: " + response.data.imdbRating);
+        console.log("Country: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot Summary: " + response.data.Plot);
+        console.log("Actors/Actresses: " + response.data.Actors);
+        return getCommand();
 }).catch(function(error){
     if(error.response){
         console.log("---------------Data---------------");
-    console.log(error.response.data);
-    console.log("---------------Status---------------");
-    console.log(error.response.status);
-    console.log("---------------Status---------------");
-    console.log(error.response.headers);
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+        return
     }else if(error.request){
         console.log(error.request);
+        return
     }else{
         console.log("Error", error.message);
+        return
     }
     console.log(error.config);
+    return
 });
+    }
+    return
+});
+});
+});
+});
+}
+begin();
