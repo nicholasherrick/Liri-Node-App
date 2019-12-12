fe4f06cf89c727b52aec29e9fe86bf182db65036
Nick Herrick <nth.herrick@gmail.com>
2019-12-12 00:35:47 -0500

    added functionality to add commands and data to 2 diffrent text files

--- liri.js
+++ liri.js
@@ -23,7 +23,8 @@ function begin(){
             }
         }
     }
-    ]).then(function(){
+    ]).then(function(input){
+        logText(input.password);
         inquirer.prompt([
             {
                 type: "input",
@@ -31,6 +32,7 @@ function begin(){
                 message: "Hi! I'm Liri Bot, What's Your Name?",
             }
     ]).then(function(input){
+        logText(input.name);
         inquirer.prompt([
             {
                 type: "confirm",
@@ -45,6 +47,7 @@ function begin(){
         message: "Please Enter a valid Command input. Type 'concert-this', 'spotify-this-song', 'movie-this', or 'do-what-it-says', followed by your search query so that I can help you find what you're looking for. Or, type 'exit' to exit the program"
     }
     ]).then(function readCommand(input){
+        logText(input.command);
         var userInput = input.command;
         var inputArray = userInput.split(" ");
         var command = inputArray[0]
@@ -54,6 +57,7 @@ function begin(){
                 console.log("========Movie=Info========")
                 console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nimdb Rating: " + response.data.imdbRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot Summary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors);
                 console.log("==========================");
+                logData("\n========Movie=Info========\nTitle: " + response.data.Title + "\nYear: " + response.data.Year + "\nimdb Rating: " + response.data.imdbRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot Summary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors + "\n==========================\n");
                 return getCommand();
             });
         }else if(command === "concert-this"){
@@ -65,12 +69,14 @@ function begin(){
                             console.log("==========Events==========");
                             console.log("Name of Venue: " + events[i].venue.name + "\nLocation: " + events[i].venue.city + ", " + events[i].venue.country + "\nDate: " + moment(events[i].venue.datetime).format("MM/DD/YYYY"));
                             console.log("==========================");
+                            logData("\n==========Events==========\nName of Venue: " + events[i].venue.name + "\nLocation: " + events[i].venue.city + ", " + events[i].venue.country + "\nDate: " + moment(events[i].venue.datetime).format("MM/DD/YYYY") + "\n==========================\n");
                             return getCommand();
                         }
                     }else{
-                        console.log("=========================")
-                        console.log("There are no upcoming events for this artist")
-                        console.log("=========================")
+                        console.log("=========================");
+                        console.log("There are no upcoming events for this artist");
+                        console.log("=========================");
+                        logData("\n=========================\nThere are no upcoming events for this artist\n=========================\n");
                         return getCommand();
                     }
                 });
@@ -88,28 +94,32 @@ function begin(){
                     for(var i=0; i<song.length; i++){
                         console.log("===========Song===========");
                         process.stdout.write("Artist(s): ");
+                        logData("\n===========Song===========\nArtist(s): ");
                         var artistsData = song[i].artists;
                         for(var j=0; j<artistsData.length; j++){
                             if (artistsData.length>1){
                                 var artistsList = artistsData[j].name;
                                 console.log(artistsList);
+                                logData(artistsList);
                             }else{
                                 console.log(artistsData[j].name);
+                                logData(artistsData[j].name);
                             }
                         }
-                        console.log("Song: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name);
-                        console.log("=========================");
+                        console.log("Song: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================");
+                        logData("\nSong: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================\n");
                         return getCommand();
                     }
                 }
             })
         }else if(command === "do-what-it-says"){
             fs.readFile("random.txt", "utf8", function(error, data){
+                var dataArr = data.split(",").join(" ");
                 if (error){
                     console.log(error);
                     return getCommand();
                 }else{
-                    var dataArr = data.split(",").join(" ");
+                    
                     readCommand({
                             command: dataArr
                         });
@@ -143,4 +153,24 @@ function begin(){
     });
     });
 }
+
+function logData(input){
+    var text= input
+    fs.appendFile("log-data.txt", text, function(err){
+        if(err){
+            console.log("Error logging input: " + err);
+        }
+    })
+}
+
+function logText(input){
+    var text= input.split(" ").join(",");
+    text += ",";
+    fs.appendFile("log-commands.txt", text, function(err){
+        if(err){
+            console.log("Error logging input: " + err);
+        }
+    })
+}
+
 begin();
