0e9498f030b7c94e042b7fe0fe44a2b465fff650
Nick Herrick <nth.herrick@gmail.com>
2019-12-12 21:37:35 -0500

    updated readme and added comment code

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Liri-Node-App
+
+![Screenshot](/assets/screenshot.png)
+
+## About
+
+This application makes use of Node.js to create a command line interface. The user must install several npm packages before the app will work. After entering a password and name, the user is given the ability to type in commands that the app will take in and run various functions to return specific data. The user can type in "movie-this" followed by a movie name to display data about the movie, "concert-this" followed by a musician/band name, "spotify-this-song" followed by a song name, and "do-what-it-says" will display the data for a song saved in the random.txt file. The app uses the spotify, bandsintown, and omdn APIs to display the data. The  app also records all the data and commands entered into 2 different .txt files. The user is able to continually use the app to make searches without reloading it again. The user can type "exit" as a command to exit the app.
+
+## Demo/Code
+
+![Demo](/assets/video.webm)
+
+![Code](/assets/code.png)
+
+## Requirements
+
+*[Node](https://nodejs.org/en/)
+
+*[Spotify API Key](https://developer.spotify.com/dashboard/)
+
+*[Spotify API](https://www.npmjs.com/package/node-spotify-api)
+
+*[Axios](https://www.npmjs.com/package/axios)
+
+*[Moment NPM](https://www.npmjs.com/package/moment)
+
+*[Dotenv](https://www.npmjs.com/package/dotenv)
+
+*[Inquirer](https://www.npmjs.com/package/inquirer)
+
+*[FS](https://www.npmjs.com/package/file-system)
+
+## Instructions
+
+1. Clone repository
+
+2. Sign up for a Spotify API Key
+
+3. create a .env file and add the spotify api keys like so:
+```
+# Spotify API keys
+
+SPOTIFY_ID=your-spotify-id
+SPOTIFY_SECRET=your-spotify-secret
+```
+4. Open the git bash terminal and navigate to the repository
+
+5. Enter ```npm init -y``` into the terminal
+
+6. Enter ```npm i dotenv axios moment inquirer fs node-spotify-api``` and wait for packages to install
+
+7. Enter ```node liri.js``` and follow onscreen prompts to use the app
+
+## Build Tools
+
+*Javascript
+
+*Git Bash Terminal
+
+*Moment.js
+
+*Node
+
+*NPM
--- liri.js
+++ liri.js
@@ -1,12 +1,17 @@
+// Listed our NPM package requirements
 require("dotenv").config();
 const axios = require('axios');
 const inquirer = require('inquirer');
 const fs = require("fs");
 const moment = require('moment');
+// Sets up the current moment for moment.js use
 moment().format();
 const Spotify = require('node-spotify-api');
+// So our app knows it needs the keys.js file
 const keys = require("./keys.js");
+// Takes the api keys from the keys.js file
 const spotify = new Spotify(keys.spotify);
+// Runs the app
 function begin(){
     inquirer.prompt([
     {
@@ -40,6 +45,7 @@ function begin(){
                 message: "Welcome " + input.name + "! You can ask me to search for data about concerts, songs and movies. Would You Like to Continue?"
             }
     ]).then(function getCommand(){
+        // This is where the app will take in commands
     inquirer.prompt([
     {
         type: "input",
@@ -47,17 +53,22 @@ function begin(){
         message: "Please Enter a valid Command input. Type 'concert-this', 'spotify-this-song', 'movie-this', or 'do-what-it-says', followed by your search query so that I can help you find what you're looking for. Or, type 'exit' to exit the program"
     }
     ]).then(function readCommand(input){
+        // sends the input to the function that logs the command to a .txt file
         logText(input.command);
+        // Takes in the command and formats it for our functions to use
         var userInput = input.command;
         var inputArray = userInput.split(" ");
         var command = inputArray[0]
         var searchTerm = inputArray.slice(1).join("+");
         if(command === "movie-this"){
             axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy").then(function(response){
+                // After we have the data, it will log it for the user to see
                 console.log("========Movie=Info========")
                 console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nimdb Rating: " + response.data.imdbRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot Summary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors);
                 console.log("==========================");
+                // Here we are sending the data to the function that will log said data to a .txt file
                 logData("\n========Movie=Info========\nTitle: " + response.data.Title + "\nYear: " + response.data.Year + "\nimdb Rating: " + response.data.imdbRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot Summary: " + response.data.Plot + "\nActors/Actresses: " + response.data.Actors + "\n==========================\n");
+                // sends the user back to the inquirer prompt that waits for a new command
                 return getCommand();
             });
         }else if(command === "concert-this"){
@@ -95,19 +106,25 @@ function begin(){
                         console.log("===========Song===========");
                         process.stdout.write("Artist(s): ");
                         logData("\n===========Song===========\nArtist(s): ");
-                        var artistsData = song[i].artists;
-                        for(var j=0; j<artistsData.length; j++){
-                            if (artistsData.length>1){
-                                var artistsList = artistsData[j].name;
-                                console.log(artistsList);
-                                logData(artistsList);
-                            }else{
-                                console.log(artistsData[j].name);
-                                logData(artistsData[j].name);
-                            }
-                        }
-                        console.log("Song: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================");
-                        logData("\nSong: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================\n");
+                        var artistsData = song[i].artists[0].name;
+                        console.log(artistsData);
+                        logData(artistsData);
+                        // Commented out because data wont display consistantly
+                        // for(var j=0; j<artistsData.length; j++){
+                        //     if (artistsData.length>1){
+                        //         var artistsList = artistsData[j].name;
+                        //         var convertedArray = [];
+                        //         convertedArray.push(artistsList);
+                        //         var artistsListString = convertedArray;
+                        //         console.log(artistsListString.toString());
+                        //         logData(artistsListString.join(" \n"));
+                        //     }else{
+                        //         console.log(artistsData[j].name);
+                        //         logData(artistsData[j].name);
+                        //     }
+                        // }
+                        console.log("Title: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================");
+                        logData("\nTitle: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name + "\n=========================\n");
                         return getCommand();
                     }
                 }
@@ -132,6 +149,7 @@ function begin(){
             return getCommand();
         }
     }).catch(function(error){
+        // catches any errors and logs them to the console
         if(error.response){
             console.log("---------------Data---------------");
             console.log(error.response.data);
@@ -154,6 +172,7 @@ function begin(){
     });
 }
 
+// functions that log information to the .txt files
 function logData(input){
     var text= input
     fs.appendFile("log-data.txt", text, function(err){
@@ -172,5 +191,5 @@ function logText(input){
         }
     })
 }
-
+// Runs the app after user inputs "node liri.js"
 begin();
